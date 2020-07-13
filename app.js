b86299e22ba2a1ebaf5545c94101b77d4982d68b
Derick Alfaro <theroadhouse@protonmail.com>
2020-07-13 11:11:09 -0400

    update and final commit

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 const express = require('express')
-const multer  = require('multer')
+const ejs = require('ejs')
+const path = require('path')
+const PORT = process.env.PORT || 3000
+const app = express()
+
+const multer = require('multer')
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
+  destination: (req, file, cb) => {
     cb(null, path.join(__dirname, 'public/uploads'))
   },
-  filename: function(req, file, cb) {
-    console.log(file);
+  filename: (req, file, cb) => {
     cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
   }
 })
-
 const upload = multer({
-  storage: storage
+  storage: storage,
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb)
+  }
 }).single('uploadImage')
 
-const ejs   = require('ejs')
-const path  = require('path')
-const PORT  = process.env.PORT || 3000
-const app   = express()
+function checkFileType(file, cb) {
+  const filetypes = /jpeg|jpg|png|gif/
 
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
+  const mimetype = filetypes.test(file.mimetype)
+
+  mimetype && extname ? cb(null, true) : cb('Error: Images Only!')
+}
 
 app.set('view engine', 'ejs')
 
@@ -31,10 +43,17 @@ app.get('/', (req, res) => res.render('index.ejs'))
 app.post('/upload', (req, res) => {
 
   upload(req, res, err => {
-    if(err){
-      res.render('index', {msg: err})
+    if (err) {
+      res.render('index', {
+        msg: err
+      })
     } else {
-      res.send('else')
+      if (req.file == undefined) {
+        res.render('index', {msg: 'Error: No File Selected'})
+      } else {
+        res.render('index', {msg: 'File Uploaded!', file: `uploads/${req.file.filename}`})
+      }
+    
     }
   })
 
